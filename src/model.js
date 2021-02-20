70e60882a0c5685e3532bccf397c0d8cfc9adc89
Oleksandr Pushkar <64772614+tauruskin@users.noreply.github.com>
2021-02-20 19:56:14 +0200

    first commit

---
+++ src/model.js
@@ -0,0 +1,17 @@
+import image from './assets/image.png'
+export const model = [
+  {
+    type: 'title', value: 'Hello world from JS'
+  },
+  {
+    type: 'text', value: 'here we go with some text'
+  },
+  {
+    type: 'columns', value: [
+      '1111111',
+      '2222222',
+      '3333333'
+    ]
+  },
+  { type: 'image', value: image }
+]
